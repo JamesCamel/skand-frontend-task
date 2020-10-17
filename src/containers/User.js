f87c4a840e1d4efd69190fa82250404c26d898e2
James Tsai <jamesscamel@gmail.com>
2020-10-17 16:11:23 +0800

    Refactor, change to React Hook

--- src/containers/User.js
+++ src/containers/User.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
 import { requestUserDetail } from "./../actions/User"
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -8,6 +6,8 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 const useStyles = makeStyles({
   root: {
@@ -26,50 +26,45 @@ const useStyles = makeStyles({
   },
 });
 
+const User = (props) => {
+  const userId = props.match.params.id
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.User)
+  React.useEffect(() => {
+    dispatch(requestUserDetail(userId))
+  }, [])
 
-class User extends React.Component {
-  componentDidMount() {
-    const userId = this.props.match.params.id
-    this.props.requestUserDetail(userId);
-  }
+  const classes = useStyles;
+  const bull = <span className={classes.bullet}>•</span>;
 
-  render () {
-    const classes = useStyles;
-    const bull = <span className={classes.bullet}>•</span>;
-    const user = this.props.user
-    console.log(user)
-    return (
-      <Card className={classes.root} variant="outlined">
-        <CardContent>
-          <Typography className={classes.title} color="textSecondary" gutterBottom>
-           id: {user.id}
-          </Typography>
-          <Typography variant="h5" component="h2">
-            {user.first_name} {bull} {user.last_name}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {user.active === true ? 'active' : 'inactive'}
-          </Typography>
-          <Typography variant="body2" component="p">
-            Slack: {user.slack_username}
-            <br />
-            Jobs: {user.jobs_count}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">
-            {user.email}
-          </Button>
-        </CardActions>
-      </Card>
-    );
-  }
+  return (
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+         id: {user.id}
+        </Typography>
+        <Typography variant="h5" component="h2">
+          {user.first_name} {bull} {user.last_name}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          {user.active === true ? 'active' : 'inactive'}
+        </Typography>
+        <Typography variant="body2" component="p">
+          Slack: {user.slack_username}
+          <br />
+          Jobs: {user.jobs_count}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">
+          {user.email}
+        </Button>
+      </CardActions>
+    </Card>
+  );
+
 }
-const mapStateToProps = state => ({ user: state.User });
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ requestUserDetail }, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(User);
+export default User
 
 
--- src/containers/UserList.js
+++ src/containers/UserList.js
@@ -1,76 +1,72 @@
 import React from "react";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
 import { requestUserList } from "./../actions/UserList";
 import MaterialTable from 'material-table';
 import _ from "lodash";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-class UserList extends React.Component {
-  componentDidMount() {
-    this.props.requestUserList();
-  }
 
-  render() {
-    const userList = this.props.userList
-    let data = []
-    if (!_.isEmpty(userList)) {
-      userList.map(user => {
-        data.push({
-          id: user.id,
-          email: user.email,
-          jobs_count: user.jobs_count,
-          active: user.active
-        })
+const UserList = (props) => {
+  const dispatch = useDispatch()
+  const userList = useSelector(state => state.UserList)
+  React.useEffect(() => {
+    dispatch(requestUserList())
+  }, [])
+
+  let data = []
+  if (!_.isEmpty(userList)) {
+    userList.map(user => {
+      data.push({
+        id: user.id,
+        email: user.email,
+        jobs_count: user.jobs_count,
+        active: user.active
       })
-    }
-
-    const columns = [
-      { title: 'ID', field: 'id' },
-      { title: 'Email', field: 'email' },
-      { title: 'Jobs Count', field: 'jobs_count', type: 'numeric' },
-      { title: 'Active', field: 'active'}
-    ]
-
-    return(
-      <MaterialTable
-        title="User List"
-        columns={[...columns]}
-        data={[...data]}        
-        actions={[
-          {
-            icon: 'pageview',
-            tooltip: 'View User',
-            onClick: (event, rowData) => {
-              this.props.history.push(`/users/${rowData.id}`)
-            }
-          },
-          {
-            icon: 'create',
-            tooltip: 'Edit User',
-            onClick: (event, rowData) => {
-              alert("You Edit " + rowData.id)
-            }
-          },
-          {
-            icon: 'delete',
-            tooltip: 'Delete User',
-            // onClick: (event, rowData) => confirm("You want to delete " + rowData.name)
-          },
-          {
-            icon: 'add',
-            tooltip: 'Add User',
-            isFreeAction: true,
-            onClick: (event) => alert("You want to add a new row")
-          }  
-        ]}
-      />
-    )
+    })
   }
+
+  const columns = [
+    { title: 'ID', field: 'id' },
+    { title: 'Email', field: 'email' },
+    { title: 'Jobs Count', field: 'jobs_count', type: 'numeric' },
+    { title: 'Active', field: 'active'}
+  ]
+
+  return(
+    <MaterialTable
+      title="User List"
+      columns={[...columns]}
+      data={[...data]}        
+      actions={[
+        {
+          icon: 'pageview',
+          tooltip: 'View User',
+          onClick: (event, rowData) => {
+            props.history.push(`/users/${rowData.id}`)
+          }
+        },
+        {
+          icon: 'create',
+          tooltip: 'Edit User',
+          onClick: (event, rowData) => {
+            alert("You Edit " + rowData.id)
+          }
+        },
+        {
+          icon: 'delete',
+          tooltip: 'Delete User',
+          // onClick: (event, rowData) => confirm("You want to delete " + rowData.name)
+        },
+        {
+          icon: 'add',
+          tooltip: 'Add User',
+          isFreeAction: true,
+          onClick: (event) => alert("You want to add a new row")
+        }  
+      ]}
+    />
+  )
+
 }
 
-const mapStateToProps = state => ({ userList: state.UserList });
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ requestUserList }, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(UserList);
+export default UserList
